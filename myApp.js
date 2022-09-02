ae9dfc81651ae4d7fa49e6eb258a7555f5fe3668
Fatih <justicestranger@gmail.com>
2022-09-03 00:06:15 +0300

    Task5: when client get the page of localhost:3000/json we serve json

--- myApp.js
+++ myApp.js
@@ -6,15 +6,21 @@ console.log("Hello World");
 // absolutePath using node global variable __dirname
 const htmlPath = __dirname + "/views/index.html"
 const cssPath = __dirname + "/public"
+const obje = {"message": "Hello json"};
 
 // Middleware of css coming from express.static
 app.use("/public",express.static(cssPath));
 
-// // app is express object and using get method in path of same directory to make response and requests.
-
+// when client enters url on main "localhost:3000/" he sees index.html and gets css of index.html with static file expression above
 app.get("/", (req, res) => {
-     //response.sendFile is a method of when get method is called client receives file from the absolutePath
+     // so here server sends index.html with res.sendFile method.
      res.sendFile(htmlPath);
+})
+
+// when client enters the url of "localhost:3000/json" he sees the json object which can be manipulated.
+app.get("/json",(req,res)=>{
+     // response is obje turned into a json format for sending
+     res.json(obje);
 })
 
 // // when client connects to server if get method called 
