c507e4abb74d59418fd0d954619e476ca251ce95
Fatih <justicestranger@gmail.com>
2022-09-02 18:44:04 +0300

    Task3: app.get usage with req and response which serves views/index.html

--- myApp.js
+++ myApp.js
@@ -3,10 +3,20 @@ let app = express();
 
 console.log("Hello World");
 
-app.get("/",function(req,res){
-     res.send('Hello Express');
+// absolutePath using node global variable __dirname
+const absolutePath = __dirname + "/views/index.html"
+
+// app is express object and using get method in path of same directory to make response and requests.
+app.get("/", (req, res) => {
+     //response.sendFile is a method of when get method is called client receives file from the absolutePath
+     res.sendFile(absolutePath)
 })
 
+// // when client connects to server if get method called 
+// app.get("/",function(req,res){
+//      //Then in response we send 'hello express to the page'
+//      res.send('Hello Express');
+// })
 
 
 
@@ -32,4 +42,5 @@ app.get("/",function(req,res){
 
 
 
- module.exports = app;
+
+module.exports = app;
