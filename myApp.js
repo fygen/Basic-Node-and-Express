7f0b29493b9222a4cfd61363e718c5fa18447599
Fatih <justicestranger@gmail.com>
2022-09-03 01:34:44 +0300

    Task8: Chaining middleware inside function using next()

--- myApp.js
+++ myApp.js
@@ -8,16 +8,16 @@ console.log("Hello World");
 // absolutePath using node global variable __dirname
 const htmlPath = __dirname + "/views/index.html"
 const cssPath = __dirname + "/public"
-const obje = {"message": "Hello json"};
+const obje = { "message": "Hello json" };
 
 // For every request write to the server console which method: get post delete , which path requested localhost:3000/ , .../json and which ip requested those.. 
-app.use(function(req,res,next){
+app.use(function (req, res, next) {
      console.log(`${req.method} ${req.path} - ${req.ip}`)
      next();
 })
 
 // Middleware of css coming from express.static
-app.use("/public",express.static(cssPath));
+app.use("/public", express.static(cssPath));
 
 // when client enters url on main "localhost:3000/" he sees index.html and gets css of index.html with static file expression above
 app.get("/", (req, res) => {
@@ -26,12 +26,21 @@ app.get("/", (req, res) => {
 })
 
 // when client enters the url of "localhost:3000/json" he sees the json object which can be manipulated.
-app.get("/json",(req,res)=>{
+app.get("/json", (req, res) => {
      // response is obje turned into a json format for sending
      console.log(process.env.MESSAGE_STYLE);
-     process.env.MESSAGE_STYLE=="uppercase"?
-     obje.message = obje.message.toUpperCase():null;      
+     process.env.MESSAGE_STYLE == "uppercase" ?
+          obje.message = obje.message.toUpperCase() : null;
      res.json(obje);
+})
+
+// client GET /now
+// chaining middleWares for learning purposes like if user permitted to interact with something 
+app.get('/now', (req, res, next) => {
+     req.time = new Date().toString();
+     next();
+}, (req, res) => {
+     res.json({ "time": req.time });
 })
 
 // // when client connects to server if get method called 
