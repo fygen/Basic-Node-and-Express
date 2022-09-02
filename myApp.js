547d91df09ba9ae3e98cb7a178badd59463632bc
Fatih <justicestranger@gmail.com>
2022-09-03 01:05:56 +0300

    Task7: server console logging the method get post delete path /json or ./ and ip of the request. Using app.use at root-level with next() for next middleware operations.

--- myApp.js
+++ myApp.js
@@ -10,6 +10,12 @@ const htmlPath = __dirname + "/views/index.html"
 const cssPath = __dirname + "/public"
 const obje = {"message": "Hello json"};
 
+// For every request write to the server console which method: get post delete , which path requested localhost:3000/ , .../json and which ip requested those.. 
+app.use(function(req,res,next){
+     console.log(`${req.method} ${req.path} - ${req.ip}`)
+     next();
+})
+
 // Middleware of css coming from express.static
 app.use("/public",express.static(cssPath));
 
@@ -22,6 +28,7 @@ app.get("/", (req, res) => {
 // when client enters the url of "localhost:3000/json" he sees the json object which can be manipulated.
 app.get("/json",(req,res)=>{
      // response is obje turned into a json format for sending
+     console.log(process.env.MESSAGE_STYLE);
      process.env.MESSAGE_STYLE=="uppercase"?
      obje.message = obje.message.toUpperCase():null;      
      res.json(obje);
