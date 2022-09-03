3ce2a5dcc7396b74908cb4fd38220c343a53f5ef
Fatih <justicestranger@gmail.com>
2022-09-03 03:22:53 +0300

    Task11: adding a new middleware for getting HTML form data. app.use added.

--- myApp.js
+++ myApp.js
@@ -1,3 +1,4 @@
+let bodyParser = require("body-parser");
 let express = require('express');
 require('dotenv').config();
 
@@ -5,27 +6,30 @@ let app = express();
 
 console.log("Hello World");
 
-// absolutePath using node global variable __dirname
+/*  absolutePath using node global variable __dirname */
 const htmlPath = __dirname + "/views/index.html"
 const cssPath = __dirname + "/public"
 const obje = { "message": "Hello json" };
 
-// For every request write to the server console which method: get post delete , which path requested localhost:3000/ , .../json and which ip requested those.. 
+/*  For every request write to the server console which method: get post delete , which path requested localhost:3000/ , .../json and which ip requested those..  */
 app.use(function (req, res, next) {
      console.log(`${req.method} ${req.path} - ${req.ip}`)
      next();
 })
 
-// Middleware of css coming from express.static
+/*  Middleware of css coming from express.static */
 app.use("/public", express.static(cssPath));
 
-// when client enters url on main "localhost:3000/" he sees index.html and gets css of index.html with static file expression above
+/* Adding a middleware for reaching the html form data */
+app.use(bodyParser.urlencoded({ extended: false }));
+
+/*  when client enters url on main "localhost:3000/" he sees index.html and gets css of index.html with static file expression above */
 app.get("/", (req, res) => {
      // so here server sends index.html with res.sendFile method.
      res.sendFile(htmlPath);
 })
 
-// when client enters the url of "localhost:3000/json" he sees the json object which can be manipulated.
+/*  when client enters the url of "localhost:3000/json" he sees the json object which can be manipulated. */
 app.get("/json", (req, res) => {
      // response is obje turned into a json format for sending
      console.log(process.env.MESSAGE_STYLE);
@@ -34,8 +38,8 @@ app.get("/json", (req, res) => {
      res.json(obje);
 })
 
-// client GET /now
-// chaining middleWares for learning purposes like if user permitted to interact with something 
+/*  client GET /now */
+/*  chaining middleWares for learning purposes like if user permitted to interact with something  */
 app.get('/now', (req, res, next) => {
      req.time = new Date().toString();
      next();
@@ -43,21 +47,29 @@ app.get('/now', (req, res, next) => {
      res.json({ "time": req.time });
 })
 
-// This get method is useful for getting the variable of the :word from client which is a parameter then send it back json format.
+/*  This get method is useful for getting the variable of the :word from client which is a parameter then send it back json format. */
 app.get('/:word/echo', (req, res, next) => {
      res.json({ "echo": req.params.word });
 })
 
-// This get method uses this convention .../name?first=firstname&last=lastname the data can be reached using req.query object 
-app.get("/name", (req, res, next)=>{
-     res.json({
-          "name": `${req.query.first} ${req.query.last}` 
-     })
-})
+/* This get method uses this convention .../name?first=firstname&last=lastname the data can be reached using req.query object */
+// app.get("/name", (req, res, next)=>{
+//      res.json({
+//           "name": `${req.query.first} ${req.query.last}` 
+//      })
+// })
 
-// // when client connects to server if get method called 
+/* This overrides the previous method by using route we dont have to specify app.post again we can use chained functions */
+app.route("/name")
+     .get((req, res, next) => {
+          res.json({
+               "name": `${req.query.first} ${req.query.last}`
+          });
+     })
+
+/* when client connects to server if get method called  */
 // app.get("/",function(req,res){
-//      //Then in response we send 'hello express to the page'
+//      /* Then in response we send 'hello express to the page' */
 //      res.send('Hello Express');
 // })
 
